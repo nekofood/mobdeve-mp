312b15447adfad9fd9effd1f2bc4e93f32e89be8
JPMPDEV <john_paul_pangan@dlsu.edu.ph>
2023-10-25 01:52:52 +0800

    Adding SlideMenu to Mainscreen

--- app/src/main/java/com/greendale/mobdeve_mp/Mainscreen.java
+++ app/src/main/java/com/greendale/mobdeve_mp/Mainscreen.java
@@ -3,12 +3,27 @@ package com.greendale.mobdeve_mp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class Mainscreen extends AppCompatActivity {
-
+    FrameLayout slideMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainscreen);
+        slideMenu = findViewById(R.id.slideMenu);
+        BottomSheetBehavior.from(slideMenu).setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        }
+    public void OpenSesame(View v)
+    {
+        BottomSheetBehavior.from(slideMenu).setState(BottomSheetBehavior.STATE_EXPANDED);
     }
-}
+    public void CloseSesame(View v)
+    {
+        BottomSheetBehavior.from(slideMenu).setState(BottomSheetBehavior.STATE_HIDDEN);
+    }
+    }
