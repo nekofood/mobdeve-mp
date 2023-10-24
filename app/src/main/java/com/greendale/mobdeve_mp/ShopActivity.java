a7fe1281c544a75e00925a642fca46d2308882af
dale <sonicroblox@gmail.com>
2023-10-24 16:07:58 +0800

    boilerplate dialog code

--- app/src/main/java/com/greendale/mobdeve_mp/ShopActivity.java
+++ app/src/main/java/com/greendale/mobdeve_mp/ShopActivity.java
@@ -1,8 +1,13 @@
 package com.greendale.mobdeve_mp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
 
 public class ShopActivity extends AppCompatActivity {
@@ -13,5 +18,42 @@ public class ShopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+
+        ImageButton[] shopButton = new ImageButton[3];
+        shopButton[0] = (ImageButton) findViewById(R.id.shopButton1);
+        shopButton[1] = (ImageButton) findViewById(R.id.shopButton2);
+        shopButton[2] = (ImageButton) findViewById(R.id.shopButton3);
+
+
+
+        for (ImageButton btn : shopButton) {
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                }
+            });
+        }
+    }
+}
+
+public class ShopFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("Purchase this item?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                });
+        return builder.create(); //TODO
     }
 }
