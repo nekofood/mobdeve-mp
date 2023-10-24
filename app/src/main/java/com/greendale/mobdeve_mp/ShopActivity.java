02d8ffb1d893c75351c5bf72cbbf3014052ca9b4
dale <sonicroblox@gmail.com>
2023-10-24 15:38:04 +0800

    more button bullshit

--- app/src/main/java/com/greendale/mobdeve_mp/ShopActivity.java
+++ app/src/main/java/com/greendale/mobdeve_mp/ShopActivity.java
@@ -3,8 +3,11 @@ package com.greendale.mobdeve_mp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class ShopActivity extends AppCompatActivity {
+
+    ImageButton shopBackBtn, shopButton1, shopButton2, shopButton3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
