aa767b07acfa2a87cd77d4bef390f50599ed8891
JPMPDEV <john_paul_pangan@dlsu.edu.ph>
2023-10-26 23:19:56 +0800

    Battle Popup returns to Mainscreen

--- app/src/main/java/com/greendale/mobdeve_mp/BattlePopup.java
+++ app/src/main/java/com/greendale/mobdeve_mp/BattlePopup.java
@@ -1,6 +1,8 @@
 package com.greendale.mobdeve_mp;
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
@@ -14,7 +16,8 @@ public class BattlePopup extends DialogFragment {
                 .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-
+                        Intent intent = new Intent(getActivity(), Mainscreen.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
                     }
                 })
                 .setNegativeButton("No", new DialogInterface.OnClickListener() {
