ac2fd218a06caa10e0d397b7dca2c2db25e1299c
dale <sonicroblox@gmail.com>
2023-10-27 20:44:23 +0800

    ok maybe we do need to go to quiz first

--- app/src/main/java/com/greendale/mobdeve_mp/MainActivity.java
+++ app/src/main/java/com/greendale/mobdeve_mp/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent i = new Intent(MainActivity.this, QuizActivity.class);
+        startActivity(i);
+        finish();
     }
 }
