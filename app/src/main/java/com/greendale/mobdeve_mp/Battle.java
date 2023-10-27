38c794689b20024daf912f31e93d14f3ba2a4330
JPMPDEV <john_paul_pangan@dlsu.edu.ph>
2023-10-27 22:41:35 +0800

    Ma

--- app/src/main/java/com/greendale/mobdeve_mp/Battle.java
+++ app/src/main/java/com/greendale/mobdeve_mp/Battle.java
@@ -3,25 +3,31 @@ package com.greendale.mobdeve_mp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.DialogFragment;
-
-import android.app.Dialog;
-import android.content.DialogInterface;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.ImageButton;
 public class Battle extends AppCompatActivity {
     ImageButton pauseButton;
+    FrameLayout battlePopup;
+    ImageButton exitMatchButton;
+    ImageButton resumeMatchButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_battle);
         pauseButton = findViewById(R.id.pauseButton);
+        battlePopup = findViewById(R.id.battlePopup);
+        exitMatchButton = findViewById(R.id.exitMatchButton);
+        resumeMatchButton = findViewById(R.id.resumeMatchButton);
+        battlePopup.setVisibility(View.GONE);
     }
     public void Pause(View view) {
-        DialogFragment frag = new BattlePopup();
-        frag.show(getSupportFragmentManager(), "Retreat");
+        battlePopup.setVisibility(View.VISIBLE);
+    }
+    public void Resume(View view) {
+        battlePopup.setVisibility(View.GONE);
+    }
+    public void Exit(View view){
+        finish();
     }
 }
