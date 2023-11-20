9762530583e55f8ed29df86534f88ca5f375e388
JPMPDEV <john_paul_pangan@dlsu.edu.ph>
2023-11-20 17:35:56 +0800

    Main Activity isQuizDone Comment

--- app/src/main/java/com/greendale/mobdeve_mp/MainActivity.java
+++ app/src/main/java/com/greendale/mobdeve_mp/MainActivity.java
@@ -11,7 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // if (isQuizDone) {
         Intent intent = new Intent(MainActivity.this, QuizActivity.class);
         startActivity(intent);
+        /*else{
+        Intent i = new Intent(MainActivity.this, Mainscreen.class);
+            startActivity(i);
+            finish();
+        }
+        * */
     }
 }
